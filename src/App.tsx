cb1f7829457ca331527e746303a4f9b97afac879
Salvatore Esposito <s.esposito@greennetwork.it>
2022-01-29 18:48:22 +0100

    useCallback + devounce _

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, ChangeEvent, useCallback, useEffect} from 'react';
+import { debounce } from 'lodash'
+
+const APIs = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [ search, setSearch] = useState<string>('')
+    const [temperature, setTemperature] = useState<number>()
+
+    const handleChangeSearch = useCallback(debounce((text: string):void => {
+        setSearch(text)
+    },2000), [])
+
+    useEffect(() => {
+        if(search === null || search === undefined) return;
+        fetch(APIs + `${search}`)
+            .then(results => results.json())
+            .then(r => setTemperature(r.current?.temperature))
+    }, [search]);
+
+    return (
+        <div>
+            <input type={'text'} onChange={(e: ChangeEvent<HTMLInputElement>) => handleChangeSearch(e.target.value)} />
+            {temperature && <p><b>Temperature:</b> {temperature}</p>}
+        </div>
+      );
 }
 
 export default App;
